b165e1c6b47099930187476da4f6566b8b061829
Gaith Hallak <gaithhallak@gmail.com>
2023-07-14 19:12:58 +0300

    Shift small int by 1 position instead of 2

--- crates/aebytecode/src/data.rs
+++ crates/aebytecode/src/data.rs
@@ -15,7 +15,7 @@ fn serialize_int(n: &BigInt) -> Bytes {
     let sign = if *n < BigInt::from(0) {NEG_SIGN} else {POS_SIGN};
     if abs < BigInt::from(SMALL_INT_SIZE) {
         let small_abs: u8 = abs.try_into().expect("is abs < SMALL_INT_SIZE ?");
-        vec![sign << 7 | small_abs << 2 | SMALL_INT]
+        vec![sign << 7 | small_abs << 1 | SMALL_INT]
     } else {
         let big_int_byte = if sign == NEG_SIGN {NEG_BIG_INT} else {POS_BIG_INT};
         let diff = (abs - BigInt::from(SMALL_INT_SIZE))
