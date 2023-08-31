73c44fb0d08a001a0058b9948921c1b1b78986d7
Gaith Hallak <gaithhallak@gmail.com>
2023-08-31 11:57:13 +0300

    Add proptest for calldata

--- crates/aebytecode/src/abi.rs
+++ crates/aebytecode/src/abi.rs
@@ -12,13 +12,33 @@ pub fn create_calldata(fun_name: &String, args: Vec<Value>) -> Result<Bytes, Ser
     Value::Tuple(vec![fun_id_val, Value::Tuple(args)]).serialize()
 }
 
-pub fn decode_calldata(fun_name: &String, calldata: Bytes) -> Result<Value, DeserErr> {
+pub fn decode_calldata(fun_name: &String, calldata: Bytes) -> Result<Vec<Value>, DeserErr> {
     let fun_id = code::symbol_identifier(fun_name);
     let fun_id_val = Value::Bytes(fun_id.to_be_bytes().to_vec());
     match Value::deserialize(&calldata)? {
         Value::Tuple(elems)
             if elems.len() == 2
-            && elems[0] == fun_id_val => Ok(elems[1].clone()),
+            && elems[0] == fun_id_val =>
+            match &elems[1] {
+                Value::Tuple(args) => Ok(args.to_vec()),
+                _ => Err(DeserErr::CalldataDecodeErr)
+            }
         _ => Err(DeserErr::CalldataDecodeErr),
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use proptest::prelude::*;
+    use crate::data::value::Value;
+
+    proptest! {
+        #[test]
+        fn calldata_round_trip(fun_name: String, args: Vec<Value>) {
+            let ser = create_calldata(&fun_name, args.clone());
+            let deser = decode_calldata(&fun_name, ser.unwrap());
+            prop_assert_eq!(deser.unwrap(), args);
+        }
     }
 }
